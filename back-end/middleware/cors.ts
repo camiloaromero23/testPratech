65ebaab81ac9b6d1dc6258891067700c59c99bb5
Camilo Romero <camiloaromero23@gmail.com>
2021-02-25 17:29:45 -0500

    Fixed CORS issue

--- back-end/middleware/cors.ts
+++ back-end/middleware/cors.ts
@@ -17,12 +17,12 @@ export const corsMiddleware = (
 	// Request headers you wish to allow
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-Requested-With,content-type',
+		'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
 	);
 
 	// Set to true if you need the website to include cookies in the requests sent
 	// to the API (e.g. in case you use sessions)
-	// res.setHeader('Access-Control-Allow-Credentials', "true");
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
 
 	// Pass to next layer of middleware
 	next();
